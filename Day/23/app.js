db8ff156d3de375cfb500125e4b6766d787edc17
alampen1@gmail.com <alampen1@gmail.com>
2025-09-16 10:34:13 +0600

    kisuna

--- Day/23/app.js
+++ Day/23/app.js
@@ -1,3 +1,8 @@
 const Input = document.getElementById("textInput");
 const addBtn = document.getElementById("addBtn");
 const Takk = documebt.getElementById("Takk");
+
+addBtn.addEventListener("click", function () {
+    const Takk = Input.value;
+    
+})
